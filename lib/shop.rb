f66666a83d65b406815d553d563e19714643049c
Hanna <hannaaikas@gmail.com>
2019-09-26 10:05:40 +0100

    Refactor: Prices split out into separate method

--- lib/shop.rb
+++ lib/shop.rb
@@ -5,16 +5,18 @@ class Shop
   def checkout(items)
     final_price = 0
     items.each_char do |letter|
-      if letter == 'A'
-        final_price += 50
-      elsif letter == 'B'
-        final_price += 30
-      elsif letter == 'C'
-        final_price += 20
-      elsif letter == 'D'
-        final_price += 15
-      end
+      final_price += price(letter)
     end
     final_price
+  end
+
+  def price(letter)
+    prices = {
+      'A' => 50,
+      'B' => 30,
+      'C' => 20,
+      'D' => 15,
+    }
+    prices[letter]
   end
 end
